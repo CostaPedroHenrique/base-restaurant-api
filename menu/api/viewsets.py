ee20e7aa05960489afedac140e3da462a388ef2b
CostaPedroHenrique <devpedrohenrique@gmail.com>
2021-08-02 21:19:01 -0300

    feat: add environment variables

--- menu/api/viewsets.py
+++ menu/api/viewsets.py
@@ -1,14 +1,28 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from .serializers import (PlateSerializer, GroupSerializer)
 
 from menu.models import (Plate, Group)
 
 
-class PlateViewSet(viewsets.ModelViewSet):
+class PlateViewSet(
+  mixins.CreateModelMixin, 
+  mixins.RetrieveModelMixin, 
+  mixins.UpdateModelMixin,
+  mixins.DestroyModelMixin,
+  mixins.ListModelMixin,
+  viewsets.GenericViewSet
+):
   queryset = Plate.objects.all()
   serializer_class = PlateSerializer
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(
+  mixins.CreateModelMixin, 
+  mixins.RetrieveModelMixin, 
+  mixins.UpdateModelMixin,
+  mixins.DestroyModelMixin,
+  mixins.ListModelMixin,
+  viewsets.GenericViewSet
+):
   queryset = Group.objects.all()
   serializer_class = GroupSerializer
